5c0b99ec898b072fbe101957be7a648724ae9a68
babubibo <55521961+babubibo@users.noreply.github.com>
2023-05-07 16:57:25 +0800

    HW

--- mygroup.c
+++ mygroup.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+        printf("Group member:\n");
         printf("408850393\n");
         printf("408850435\n");
         printf("408850443\n");
